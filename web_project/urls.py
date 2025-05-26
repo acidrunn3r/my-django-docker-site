0dfadef71aeffc155b277d46508e0fa272f48f15
Alexander Lipov <142092955+acidrunn3r@users.noreply.github.com>
2025-05-27 02:06:31 +0300

    Update urls.py

--- web_project/urls.py
+++ web_project/urls.py
@@ -26,13 +26,11 @@ urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
-# Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
-# Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
