2cf7542fe3a778a962039ddcf79b5985d65bfc9e
Alexander Lipov <142092955+acidrunn3r@users.noreply.github.com>
2025-05-27 01:55:42 +0300

    Update forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,14 +1,21 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
+import re
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    
+
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not re.match(r'^[a-zA-Z0-9_]+$', username):
+            raise ValidationError("Разрешены только латинские буквы, цифры и подчёркивания.")
+        return username
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Email')
